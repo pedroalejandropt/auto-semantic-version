edf47d453367c6572a579b50d1877e58fe1e788e
Pedro Alejandro Pacheco Tripi <pedro.alejandro.pt@gmail.com>
2023-06-27 18:58:45 +0200

    Adding Models

---
+++ dist/models/Commit.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Commit = void 0;
+class Commit {
+    constructor(hash, msg) {
+        this.Hash = hash;
+        this.Msg = msg;
+    }
+}
+exports.Commit = Commit;
---
+++ dist/models/Tag.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Tag = void 0;
+class Tag {
+    // Under construnction
+    constructor() { }
+}
+exports.Tag = Tag;
