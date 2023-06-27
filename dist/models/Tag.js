b73886864cd63826ce881cd9f655bd571fdb4200
Pedro Alejandro Pacheco Tripi <pedro.alejandro.pt@gmail.com>
2023-06-27 19:38:15 +0200

    Update JavaScript Files

--- dist/models/Tag.js
+++ dist/models/Tag.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Tag = void 0;
 class Tag {
-    // Under construnction
-    constructor() { }
+    constructor(label) {
+        this.IsRelease = () => this.Label.toLowerCase().includes('release');
+        this.Label = label;
+    }
 }
 exports.Tag = Tag;
--- dist/services/GitCommandService.js
+++ dist/services/GitCommandService.js
@@ -20,15 +20,17 @@ class GitCommandService {
         return new Commit_1.Commit(hash, msg);
     }
     async getTags() {
-        let line = 'git tag -l --pretty=format:"&$& %H ||| %s"';
-        let output = (await (0, ExecCommand_1.cmd)(line)).split('&$&');
-        let tags = output.map(() => new Tag_1.Tag());
+        let line = `git tag --list --format='%(refname:short)' | sed '$!s/$/|||/'`;
+        let output = (await (0, ExecCommand_1.cmd)(line)).split('|||');
+        let tags = output.map((label) => new Tag_1.Tag(label));
         return tags;
     }
-    async getLastTag() {
-        let line = 'git tag -l -1 --pretty=format:"%H ||| %s"';
-        let output = (await (0, ExecCommand_1.cmd)(line)).split('|||');
-        return new Tag_1.Tag();
+    async getLastTag(release) {
+        let line = (release) ?
+            `git tag --list "*release*" --format='%(refname:short)' | sed '$!s/$/|||/'` :
+            `git tag --list --format='%(refname:short)' | sed '$!s/$/|||/'`;
+        let label = (await (0, ExecCommand_1.cmd)(line)).split('|||').pop();
+        return new Tag_1.Tag(label);
     }
 }
 exports.GitCommandService = GitCommandService;
