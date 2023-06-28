4e57e050833ccef724668c5e9948b80c21d1322a
Pedro Alejandro Pacheco Tripi <pedro.alejandro.pt@gmail.com>
2023-06-28 15:19:39 +0200

    feat (version): adding version service

---
+++ src/services/VersionService.ts
@@ -0,0 +1,39 @@
+import { Tag } from '../models/Tag';
+import { Version } from '../models/Version';
+import { IVersionService } from './IVersionService';
+import { GitCommandService } from './GitCommandService';
+
+export class VersionService implements IVersionService {
+
+    _gitCommandService: GitCommandService;
+    
+    constructor() {
+        this._gitCommandService = new GitCommandService();
+    }
+    
+    async buildVersion() : Promise<Version> {
+        let tag = await this._gitCommandService.getLastTag();
+        let newVersion = this.increaseVersion(tag);
+        
+        return newVersion;
+    }
+
+    increaseVersion = (tag: Tag) : Version => {
+        let commitType = ''
+        let [major, minor, patch] =  tag.destructureTag(); 
+
+        switch (commitType) {
+            case 'major':
+                major += 1
+                break;
+            case 'minor':
+                minor += 1
+            default:
+                patch += 1
+                break;
+        }
+
+        return new Version(major, minor, patch);
+    }
+
+}
